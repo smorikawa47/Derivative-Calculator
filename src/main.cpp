15349904db8811fa99127fc4050fd5df770ae035
smorikawa47 <80607636+smorikawa47@users.noreply.github.com>
2022-03-18 03:24:34 -0700

    optimized code

--- src/main.cpp
+++ src/main.cpp
@@ -11,52 +11,52 @@ void Menu() { //A simple menu so the user has a choice of what they want to do.
 }
 
 int main() {
-    string derivatives[ARSize];
+    string derivatives;
     string derivative;
-     char choice;
-     bool quit = false;
-     while (!quit) { //run until the user closes the application
-         Menu();
-         cout << "Your choice: ";
-         cin >> choice;
-         choice = toupper(choice);                  //converts choice character into an uppercase letter.
-         cout << endl;
-         switch (choice) { //corresponds to the menu
-            case 'A': {
-                string polynomial = SetFunction();
-                derivatives[0] = polynomial;
-                int degree = degreeReturn(polynomial);
+    char choice;
+    bool quit = false;
+    while (!quit) { //run until the user closes the application
+        Menu();
+        cout << "Your choice: ";
+        cin >> choice;
+        choice = toupper(choice);                  //converts choice character into an uppercase letter.
+        cout << endl;
+        switch (choice) { //corresponds to the menu
+        case 'A': {
+            string polynomial = SetFunction();
+            derivatives = polynomial;
+            int degree = degreeReturn(polynomial);
 
-                for (int i = 0; i < degree; i++) { //this takes all the derivatives of the set polynomial, so it takes the first, second, ect..
-                    derivatives[0] = Deriv_Calculation(derivatives[0]);
-                }
-                
-                cout << endl;
-                break;
+            for (int i = 0; i < degree; i++) { //this takes all the derivatives of the set polynomial, so it takes the first, second, ect..
+                derivatives = Deriv_Calculation(derivatives);
             }
-            case 'P':
-                productrule();
-                break;
-            case 'Q':
-                quotientrule();
-                break;
-            case 'E':
-                exponentrule();
-                break;
-            case 'C':
-                chainrule();
-                break;
-            case 'S':
-                sumrule();
-                break;
-            case 'O':
-                constantrule();
-                break;
-            case 'T':
-                quit = true;
-                break;
-         }
-     }
+            
+            cout << endl;
+            break;
+        }
+        case 'P':
+            productrule();
+            break;
+        case 'Q':
+            quotientrule();
+            break;
+        case 'E':
+            exponentrule();
+            break;
+        case 'C':
+            chainrule();
+            break;
+        case 'S':
+            sumrule();
+            break;
+        case 'O':
+            constantrule();
+            break;
+        case 'T':
+            quit = true;
+            break;
+        }
+    }
     return 0;
 }
 
