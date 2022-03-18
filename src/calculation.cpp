270f6156acade67036d735eaf6d7d09deac8a815
smorikawa47 <80607636+smorikawa47@users.noreply.github.com>
2022-03-17 22:52:41 -0700

    optimized code in calculation.cpp

--- src/calculation.cpp
+++ src/calculation.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 const int ARSize = 10;
 
-string SetFunction() {//A simple function asking the user to input a polynomial. Input must be in a strict format for the program to read it correctly.
+string SetFunction() { //A simple function asking the user to input a polynomial. Input must be in a strict format for the program to read it correctly.
     string polynomial;
     while (cin.get() != '\n');
     cout << "Enter a polynomial function for example (9x^3+0x^2+7x^1+6)\nYou must include a number for all coefficeints even if it is 0, this includes the final constant\n: "; //Must input polynomial in the same style as the example.
@@ -23,18 +23,19 @@ int degreeReturn(string polynomial) {
 string Deriv_Calculation(string polynomial) {
     string originalPolynomial = polynomial;
     string derivative, derivativeComponents, derivativeDegrees;
-    int degree = stoi(polynomial.substr(polynomial.find_first_of("x") + 2));
+    int degree = stoi(polynomial.substr(polynomial.find_first_of("x") + 2)); // In 9x^3+0x^2+7x^1+6, it refers to 3 because polynomial.find_first_of("x") is x and x's index + 2 is 3
     string polynomialValues[ARSize];
     double derivativeValues[ARSize];
 
-    for (int i = degree; i != -1; i--) { //Loops until the polynomail values have stored all the coefficients of the polynomial.
-        if (polynomial.size() < 3) { //once polynomial gets to a point without 'x', we can no longer use 'x as a reference to find the coefficients'
-            polynomialValues[i] = polynomial.substr(polynomial.find_first_of("+") + 1, polynomial.length()); //Takes all the values after the '+' sign, this is the constant value of the polynomial.
+    for (int i = degree; i != -1; i--) {                         //Loops until the polynomail values have stored all the coefficients of the polynomial.
+        if (polynomial.size() == 1) {                            //once polynomial gets to a point without 'x', we can no longer use 'x as a reference to find the coefficients'
+            // polynomialValues[i] = polynomial.substr(polynomial.find_first_of("+") + 1, polynomial.length()); 
+            polynomialValues[i] = polynomial;                    //Takes all the values after the '+' sign, this is the constant value of the polynomial.
         }
         else
         {
             polynomialValues[i] = polynomial.substr(0, polynomial.find_first_of("x"));                       //Use 'x' as a reference to find the coefficients of the polynomial.
-            polynomial = polynomial.substr(polynomial.find_first_of("x") + 4, polynomial.length());          //turn polynomial into a substring which doesn't include the first 'x' so we can use the first 'x' of the substring as a reference.
+            polynomial = polynomial.substr(polynomial.find_first_of("x") + 4, polynomial.length());          //turn polynomial into a substring which doesn't include the first 'x' so we can use the first 'x' of the substring as a reference. For the first iteration of the polynomial = 9x^3+0x^2+7x^1+6, polynomial becomes 0x^2+7x^1+6
         }
     }
 
@@ -45,7 +46,7 @@ string Deriv_Calculation(string polynomial) {
         if(i == 1) {                                             //if the degree is one, do not include the '+' because there is no value after it.
             derivative = derivative + to_string(derivativeValues[i]);
         }
-        else { //otherwise include the '+' because there is a value after it.
+        else {                                                   //otherwise include the '+' because there is a value after it.
             derivative = derivative + to_string(derivativeValues[i]) + derivativeComponents;
         }
     }
